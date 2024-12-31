5fb8cec54024599e9bba03ae947bafae8175fe3b
ratheeshkumar25 <ratheeshgopinadhkumar@gmail.com>
2025-01-01 00:14:27 +0530

    configfile added

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,1 +1,10 @@
 package config
+
+type Config struct {
+	Host     string `mapstructure:"HOST"`
+	User     string `mapstructure:"USER"`
+	Password string `mapstructure:"PASSWORD"`
+	Database string `mapstructure:"DBNAME"`
+	Port     string `mapstructure:"PORT"`
+	Sslmode  string `mapstructure:"SSL"`
+}
